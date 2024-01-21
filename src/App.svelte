8b8016ba831b75be4d3a548543c971998f019e52
JulianFP <julian@partanengroup.de>
2024-01-21 16:18:54 +0100

    initial login screen layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,39 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, DarkMode, Label, Input, Button } from 'flowbite-svelte';
+  let names = [ "a", "b" ];
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div class="min-h-screen bg-white dark:bg-slate-950">
+  <Navbar  >
+    <NavBrand href="/">
+      <!--TODO add icon like this: <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />-->
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Project W</span>
+    </NavBrand>
+    <NavHamburger  />
+    <NavUl >
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/pricing">Contact</NavLi>
+      <NavLi href="/contact">Github</NavLi>
+      <img src="assets/github-favicon-dark.svg" class="me-3 h-6 sm:h-9" alt="Github Logo" />
+    </NavUl>
+  </Navbar>
+  <main class="w-full mx-auto">
+    <DarkMode />
+    <form>
+      <div class="mb-6">
+        <Label for="email" class="mb-2">Email address</Label>
+        <Input type="email" id="email" placeholder="alice@example.com" required />
+      </div>
+      <div class="mb-6">
+        <Label for="password" class="mb-2">Password</Label>
+        <Input type="password" id="password" placeholder="••••••••••••" required />
+      </div>
+      <Button type="submit">Login</Button>
+    </form>
+    {#each names as name}
+      <p class="text-base text-gray-900 dark:text-white">{name}</p>
+    {/each}
+  </main>
+</div>
