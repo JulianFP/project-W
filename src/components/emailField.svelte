21473f1007abe41e692a5b966d53b7a68558aa0a
JulianFP <julian@partanengroup.de>
2024-02-02 02:49:57 +0100

    emailField: add autocomplete attribute for better autofill support

--- src/components/emailField.svelte
+++ src/components/emailField.svelte
@@ -11,5 +11,5 @@
 
 <div class="mb-6">
   <Label for="email" color={error ? "red" : "gray"} class="mb-2">Email address</Label>
-  <Input type="email" id="email" color={error ? "red" : "base"} placeholder="alice@example.com" required bind:value={value} on:input={disableError}/>
+  <Input type="email" id="email" color={error ? "red" : "base"} placeholder="alice@example.com" autocomplete="on" required bind:value={value} on:input={disableError}/>
 </div>
