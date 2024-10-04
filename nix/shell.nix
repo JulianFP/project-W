47f48d5aa4d17df62e1ef5479dea8dfb3eb14e38
JulianFP <julian@partanengroup.de>
2024-10-04 20:34:09 +0200

    Add pre-commit with biome formatter Also update nix shell to make it compatible

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,7 +1,18 @@
 { pkgs ? import <nixpkgs> { } }:
+let
+  wrappedPreCommit = (pkgs.buildFHSEnv {
+    name = "wrapped pre-commit";
+    pname = "pre-commit";
+    targetPkgs = pkgs: [
+      pkgs.pre-commit
+    ];
+    runScript = "pre-commit";
+  });
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs_20
     corepack_20
+    wrappedPreCommit
   ];
 }
